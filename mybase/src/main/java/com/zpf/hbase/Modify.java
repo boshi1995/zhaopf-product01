1ed5fbe1d89b2268cfa72ff3aa24dfff0f29d284
zhaopf-03 <wangyizpf@126.com>
2020-12-14 20:06:05 +0800

    自己修改

--- mybase/src/main/java/com/zpf/hbase/Modify.java
+++ mybase/src/main/java/com/zpf/hbase/Modify.java
@@ -4,5 +4,6 @@ public class Modify {
     public static void main(String[] args) {
         System.out.println("新的分支");
         System.out.println("1111111");
+        System.out.println("新的修改222");
     }
 }
